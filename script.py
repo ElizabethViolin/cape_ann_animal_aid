bc2df854e6377e64f90246c217262aca621dc18b
ElizabethViolin <elizabethtaylorjoy@gmail.com>
2024-07-08 20:19:18 -0400

    run script every 5 seconds. if returns true, play alarm sound

--- script.py
+++ script.py
@@ -1,36 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
+import simpleaudio as sa
+import time
 
 # Define the URL of the iframe to check
 iframe_url = 'https://ws.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Dog&gender=A&agegroup=All&location=&site=&onhold=N&orderby=Name&colnum=2&css=http://ws.petango.com/WebServices/adoptablesearch/css/styles.css&authkey=8cyw3mw2bj6lvdg7nxfqmhvq33fvuxnyxotje5j7fkjynq0wio&recAmount=&detailsInPopup=Yes&featuredPet=Include&stageID='
 
-try:
-    # Send a GET request to fetch the HTML content of the iframe
-    response = requests.get(iframe_url)
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
+# Path to the alert sound file
+alert_sound_path = '/Users/elizabethtaylor/project/cape_ann_animal_aid/alert-sound.wav' 
+
+def check_for_andy():
+    try:
+        # Send a GET request to fetch the HTML content of the iframe
+        response = requests.get(iframe_url)
         
-        # Find all <a> elements
-        links = soup.find_all('a')
-        
-        # Check each <a> tag within these <div> elements
-        for link in links:
-            href = link.get('href', '')
-            link_text = link.text.strip()
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
             
-            # Check if the href attribute contains the JavaScript function
-            if 'javascript:poptastic' in href:
-                # Check if the text contains "Andy"
-                if 'Andy' in link_text:
-                    print(True)
-                    break
+            # Find all <a> elements
+            links = soup.find_all('a')
+            
+            # Check each <a> tag within these <div> elements
+            for link in links:
+                href = link.get('href', '')
+                link_text = link.text.strip()
+                
+                # Check if the href attribute contains the JavaScript function
+                if 'javascript:poptastic' in href:
+                    # Check if the text contains "Andy"
+                    if 'Andy' in link_text:
+                        print(True)
+                        wave_obj = sa.WaveObject.from_wave_file(alert_sound_path)
+                        play_obj = wave_obj.play()
+                        play_obj.wait_done()
+                        return True
+            else:
+                # If no link with "Andy" is found
+                print(False)
+                return False
         else:
-            # If no link with "Andy" is found
-            print(False)
-    else:
-        print('Failed to fetch the iframe content. Status code:', response.status_code)
-except Exception as e:
-    print('An error occurred:', str(e))
+            print('Failed to fetch the iframe content. Status code:', response.status_code)
+            return False
+    except Exception as e:
+        print('An error occurred:', str(e))
+        return False
+
+while True:
+    if check_for_andy():
+        break
+    time.sleep(5)
